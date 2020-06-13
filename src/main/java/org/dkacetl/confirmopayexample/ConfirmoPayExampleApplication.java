d7354cbea8a80987952fa25d280c5c67835f7ccf
dkacetl <dkacetl@gmail.com>
2020-06-14 00:00:36 +0200

    init

---
+++ src/main/java/org/dkacetl/confirmopayexample/ConfirmoPayExampleApplication.java
@@ -0,0 +1,13 @@
+package org.dkacetl.confirmopayexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmoPayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmoPayExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dkacetl/confirmopayexample/ConfirmoPayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dkacetl.confirmopayexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmoPayExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
