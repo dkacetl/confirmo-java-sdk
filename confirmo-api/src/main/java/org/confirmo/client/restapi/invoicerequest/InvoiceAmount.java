cb27c6a2e5861b5e84b56f6f4213266ae12e92f0
David Kacetl <david.kacetl@zf.com>
2020-06-18 12:39:36 +0200

    Project decomposition into multiple modules

--- confirmo-api/src/main/java/org/confirmo/client/restapi/invoicerequest/InvoiceAmount.java
+++ confirmo-api/src/main/java/org/confirmo/client/restapi/invoicerequest/InvoiceAmount.java
@@ -1,5 +1,8 @@
 package org.confirmo.client.restapi.invoicerequest;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class InvoiceAmount {
 
     private String amount;
--- confirmo-pay-example/src/test/java/org/confirmo/appexample/ConfirmoPayExampleApplicationTests.java
+++ confirmo-pay-example/src/test/java/org/confirmo/appexample/ConfirmoPayExampleApplicationTests.java
@@ -2,8 +2,10 @@ package org.confirmo.appexample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:/fake-credentials.properties")
 class ConfirmoPayExampleApplicationTests {
 
 	@Test
